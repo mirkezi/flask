1bcc46b65e61e4fc0ff9f723ceba185972153d35
mirkezi <32173063+mirkezi@users.noreply.github.com>
2017-09-23 14:14:48 +0200

    FLASK

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def profile():
     return render_template('profile.html')
 
 
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+
+@app.route('/terminal')
+def terminal():
+    return render_template('terminal.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
