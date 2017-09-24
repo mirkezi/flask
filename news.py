2b7f7cf693afb34556c6ca1aed8bc3952cf20178
mirkezi <32173063+mirkezi@users.noreply.github.com>
2017-09-24 18:14:30 +0200

    flask

--- news.py
+++ news.py
@@ -6,6 +6,13 @@ def News():
             'body': 'Ciao a tutti. CodeFriends è una piattaforma innovativa per gli utenti che vogliono condividere il proprio lavoro',
             'author': 'Mirko Venali',
             'create_date': '24/09/2017'
+        },
+        {
+            'id': 2,
+            'title': 'Python',
+            'body': 'Se avete qualsiasi domanda sulla programmazione rivolgetevi a dj ise detto isihouse',
+            'author': 'Il creatore',
+            'create_date': '24/09/2017'
         }
     ]
 
