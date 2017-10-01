d1d1176a278c325c6d4c3f65e30b4dd8beb6a7c5
mirkezi <32173063+mirkezi@users.noreply.github.com>
2017-10-01 16:09:10 +0200

    flask

--- main.py
+++ main.py
@@ -34,5 +34,9 @@ def forum():
 def terminal():
     return render_template('news.html', news=News)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
